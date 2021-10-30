5e8ede11374acd0d0ba47c57b5eb3e9e0ed0eecc
Kenneth <kduenascontreras@gmail.com>
2021-10-30 02:40:39 -0500

    change name componentes to components

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,8 +1,8 @@
 import React, {useEffect,useState} from 'react'
 import axios from 'axios';
 import URL from '../../url.json'
-import LoadingComponent from '../../componentes/loadingComponent/loadingComponent'
-import ComponentGraphicLineDivisas from '../../componentes/GraphicLineDivisas/GraphicLineDivisas'
+import LoadingComponent from '../../components/loadingComponent/loadingComponent'
+import ComponentGraphicLineDivisas from '../../components/GraphicLineDivisas/GraphicLineDivisas'
 
 
 
